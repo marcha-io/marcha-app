2199b2b6bb2b1adf03f37caadd79f69b769779dc
Maaz Sattar <111438089+maazsattar@users.noreply.github.com>
2024-03-07 13:26:48 +0500

    google firebase initialised, created categories, fetching them from firebase in AddPostScreen

--- Apps/Components/AddPostScreen.jsx
+++ Apps/Components/AddPostScreen.jsx
@@ -1,7 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { app } from '../../firebaseConfig';
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 export default function AddPostScreen() {
+
+  const db = getFirestore(app);
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    getCategoryList();
+  },[])
+
+  const getCategoryList = async() => {
+    const querySnapshot=await getDocs(collection(db, 'Category'));
+    querySnapshot.forEach((doc)=>{
+      console.log("Docs:",doc.data());
+      setCategoryList(categoryList=>[...categoryList,doc.data()])
+    })
+  }
+
   return (
     <View>
       <Text>AddPostScreen</Text>
