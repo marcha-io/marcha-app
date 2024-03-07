43853c84b8264bee43ea099fc9d26783a5881849
Maaz Sattar <111438089+maazsattar@users.noreply.github.com>
2024-03-07 14:52:40 +0500

    Can upload product with: name, description, price and category

--- Apps/Components/AddPostScreen.jsx
+++ Apps/Components/AddPostScreen.jsx
@@ -1,7 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { Formik } from 'formik';
+import {Picker} from '@react-native-picker/picker';
+
 export default function AddPostScreen() {
 
   const db = getFirestore(app);
@@ -12,6 +15,7 @@ export default function AddPostScreen() {
   },[])
 
   const getCategoryList = async() => {
+    setCategoryList([]);
     const querySnapshot=await getDocs(collection(db, 'Category'));
     querySnapshot.forEach((doc)=>{
       console.log("Docs:",doc.data());
@@ -20,8 +24,51 @@ export default function AddPostScreen() {
   }
 
   return (
-    <View>
-      <Text>AddPostScreen</Text>
+    <View className="p-10">
+      <Text className="text-2xl font-bold mt-8 text-gray-800">New Product</Text>
+      <Text className="mt-1 mb-2 text-sm leading-6 text-gray-600">This information will be displayed publicly.</Text>
+      <Formik initialValues={{name:'', desc:'', category:'', price:'' }} onSubmit={value=>console.log(value)}>
+        {({handleChange, handleBlur, handleSubmit, values})=>(
+          <View>
+            <TextInput
+              className="mt-3 border border-gray-300 rounded-md py-3 px-4 bg-gray-200 text-gray-600 text-sm"
+              placeholder="Name"
+              values={values.name}
+              onChangeText={handleChange('name')}
+            />
+            <TextInput
+              className="mt-3 border border-gray-300 h-28 rounded-md py-3 px-4 bg-gray-200 text-gray-600 text-sm"
+              placeholder="Description"
+              multiline={true}
+              values={values.desc}
+              onChangeText={handleChange('desc')}
+            />
+            <TextInput
+              className="mt-3 border border-gray-300 rounded-md py-3 px-4 bg-gray-200 text-gray-600 text-sm"
+              placeholder="Price"
+              values={values.price}
+              keyboardType='numeric'
+              onChangeText={handleChange('price')}
+            />
+            <Picker
+              selectedValue={values.category}
+              onValueChange={handleChange('category')}
+              itemStyle={{ borderColor: "#d1d5db", backgroundColor:"#e5e7eb", borderWidth: 1, marginTop:12, borderRadius:6}}
+
+            >
+              {categoryList&&categoryList.map((item,index)=>(
+                <Picker.Item
+                  key={index}
+                  label={item.name}
+                  value={item.name} />
+              ))}
+            </Picker>
+            <TouchableOpacity onPress={handleSubmit} className="bg-marcha p-4 mt-3 rounded-md">
+              <Text className="text-white text-center text-base font-bold">Submit</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
     </View>
   )
 }
