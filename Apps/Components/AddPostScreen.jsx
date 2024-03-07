ad7baa59a8f553d54df6cc317d8ed29d7665d9fa
Maaz Sattar <111438089+maazsattar@users.noreply.github.com>
2024-03-07 16:01:31 +0500

    single image upload with validations

--- Apps/Components/AddPostScreen.jsx
+++ Apps/Components/AddPostScreen.jsx
@@ -1,11 +1,13 @@
-import { View, Text, TextInput, Button, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Keyboard, Image, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 import { Formik } from 'formik';
 import {Picker} from '@react-native-picker/picker';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function AddPostScreen() {
+  const [image, setImage] = useState(null);
 
   const db = getFirestore(app);
   const [categoryList, setCategoryList] = useState([]);
@@ -23,14 +25,64 @@ export default function AddPostScreen() {
     })
   }
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const onSubmitMethod = (value) => {
+    value.image = image;
+    console.log(value)
+  }
+
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <ScrollView onPress={Keyboard.dismiss} accessible={false}>
       <View className="p-10">
         <Text className="text-2xl font-bold mt-8 text-gray-800">New Product</Text>
         <Text className="mt-1 mb-2 text-sm leading-6 text-gray-600">This information will be displayed publicly.</Text>
-        <Formik initialValues={{name:'', desc:'', category:'', price:'' }} onSubmit={value=>console.log(value)}>
-          {({handleChange, handleBlur, handleSubmit, values})=>(
+        <Formik
+          initialValues={{name:'', desc:'', category:'', price:'' }}
+          onSubmit={value=>onSubmitMethod(value)}
+          validate={(values) => {
+            const errors = {}
+            if (!values.name)
+            {
+              console.log("Name is required");
+              errors.name = 'Name is required';
+            }
+            if (!values.image) {
+              console.log("Image is required");
+              errors.image = 'Image is required';
+            }
+            if (!values.desc) {
+              console.log("Description is required");
+              errors.desc = 'Description is required';
+            }
+            if (!values.category) {
+              console.log("Category is required");
+              errors.category = 'Category is required';
+            }
+            return errors
+            }}>
+          {({handleChange, handleBlur, handleSubmit, values, errors})=>(
             <View>
+              <TouchableOpacity onPress={pickImage}>
+                {image?
+                  <Image source={{uri:image}} className="mt-3 h-44 w-44 rounded-md border border-gray-400"/>
+                  : <Image source={require('./../../assets/images/placeholder.jpeg')} className="mt-3 h-44 w-44 rounded-md border border-gray-400"/>
+                }
+              </TouchableOpacity>
               <TextInput
                 className="mt-3 border border-gray-300 rounded-md h-12 px-4 bg-gray-200 text-gray-600 text-sm"
                 placeholder="Name"
@@ -71,6 +123,6 @@ export default function AddPostScreen() {
           )}
         </Formik>
       </View>
-    </TouchableWithoutFeedback>
+    </ScrollView>
   )
 }
